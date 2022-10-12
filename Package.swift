f2b8d59f66de22960d543cab6fa67a655975d21e
2222min <54797763+2222min@users.noreply.github.com>
2022-10-12 16:42:05 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-    .package(url: "https://github.com/2222min/RxExpect.git", .branch(from: "master")),
+    .package(url: "https://github.com/2222min/RxExpect.git", .branch("master")),
   ],
   targets: [
     .target(name: "RxViewController", dependencies: ["RxSwift", "RxCocoa"]),
